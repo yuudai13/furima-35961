e70fa37d6556903486410fd718fd7e2567cdd540
yuudai13 <rnjyc250@gmail.com>
2021-06-30 12:27:06 +0900

    itemとpurchaseモデルのコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 validatable :nickname, :email, :encrypted_password, :last_name_full_width, :first_name_full_width, :last_name_kana_full_width, :first_name_kana_full_width, :birthday, 
-has_many :items, dependent: :destroy
-has_many :purchases, dependent: :destroy
+# has_many :items, dependent: :destroy
+# has_many :purchases, dependent: :destroy
 end
