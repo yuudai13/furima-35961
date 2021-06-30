08d3ab89ee334c03c982caceea728855e10fb79e
yuudai13 <rnjyc250@gmail.com>
2021-06-30 12:13:16 +0900

    userテーブルにValidationとhas_maeyを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+validatable :nickname, :email, :encrypted_password, :last_name_full_width, :first_name_full_width, :last_name_kana_full_width, :first_name_kana_full_width, :birthday, 
+has_many :items, dependent: :destroy
+has_many :purchases, dependent: :destroy
 end
