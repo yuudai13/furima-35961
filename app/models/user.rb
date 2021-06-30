f556bb79a55d1f58464fcd7ba10257c79f878bbf
yuudai13 <rnjyc250@gmail.com>
2021-06-30 16:02:22 +0900

    半角英数パスワードのvalidationを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-validatable :nickname, :email, :encrypted_password, :last_name_full_width, :first_name_full_width, :last_name_kana_full_width, :first_name_kana_full_width, :birthday, 
+validatable :nickname, :email, :encrypted_password, :last_name_full_width, :first_name_full_width, :last_name_kana_full_width, :first_name_kana_full_width, :birthday, presence: :true
+
+PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates_format_of :password, with: PASSWORD_REGEX, message:
+
 # has_many :items, dependent: :destroy
 # has_many :purchases, dependent: :destroy
 end
