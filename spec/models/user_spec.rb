bedbfc73cb65b8558cab3d75c152d9a7fb66205e
yuudai13 <rnjyc250@gmail.com>
2021-07-01 21:21:49 +0900

    nickname,emailテストコード記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Nickname can't be blank"
+    end
+    it 'emailが空では登録できない' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+  end
 end
