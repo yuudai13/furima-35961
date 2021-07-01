a33bfada984a27a84341bf9c06dc4824c3c8d65a
yuudai13 <rnjyc250@gmail.com>
2021-07-01 12:18:53 +0900

    validates修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-validatable :nickname, :email, :encrypted_password, :last_name_full_width, :first_name_full_width, :last_name_kana_full_width, :first_name_kana_full_width, :birthday, presence: :true
 
-PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-validates_format_of :password, with: PASSWORD_REGEX, message:
-
+  with_options presence: true do
+    validates :nickname
+    validates :last_name_full_width
+    validates :first_name_full_width
+    validates :last_name_kana_full_width
+    validates :first_name_kana_full_width
+    validates :birthday
+  end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+end
 # has_many :items, dependent: :destroy
 # has_many :purchases, dependent: :destroy
-end
