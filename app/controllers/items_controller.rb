9d7ea37deff0e4b37927027c8c7b4f64360f236b
yuudai13 <rnjyc250@gmail.com>
2021-07-03 16:54:28 +0900

    商品出品入力ページに遷移出来るようにした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,20 @@
 class ItemsController < ApplicationController
 
 def index
+  @items = Item.all
 end
 
+def new
+  @items = Item.all
+end
+
+def create
+  Item.create(items_params)
+end
+
+private
+def items_params
+  prams.require(:items).permit(:name, :description, :category_id, :condition_id, :delivery_charge_id, :delivery_source_id, :shipping_days_id, :price).merge(user_id: curent_user.id)
+end
 end
 
---
+++ app/models/item.rb
@@ -0,0 +1,6 @@
+class Item < ApplicationRecord
+
+  belongs_to :user
+  has_one :purchase
+
+end
