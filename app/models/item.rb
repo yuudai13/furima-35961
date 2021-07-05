590878afe953a42e67b80fb23346a9914f5feaa7
yuudai13 <rnjyc250@gmail.com>
2021-07-05 12:23:58 +0900

    配送元の都道府県のプルダウン実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,14 +7,17 @@ class Item < ApplicationRecord
   has_one :purchase
   has_one_attached :image
 
+validates :delivery_source_id, numericality: { other_than: 0 }
+
   with_options presence: true do
     validates :category
     validates :condition
     validates :delivery_charge
+    validates :delivery_source
   end
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :condition_id
-    validates :Delivery_charge_id
+    validates :delivery_charge_id
   end
 end
