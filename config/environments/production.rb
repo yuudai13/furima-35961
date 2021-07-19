f69213cc725fe6b3fa96b75f79f8d41ace038213
yuudai13 <rnjyc250@gmail.com>
2021-07-19 19:09:05 +0900

    herokuで動くように修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "wss://【furima-35961】.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://【furima-35961】.herokuapp.com', 'http://【Herokuアプリ名】.herokuapp.com']
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
